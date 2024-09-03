5b439b6995a815c022f571e4c2c22df679318252
ranggi.wiharja <ranggi.wiharja@solusi247.com>
2024-09-03 09:35:35 +0000

    Added users test case

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+
+class UserTestCase(APITestCase):
+    def test_create_user(self):
+        url = reverse('user-list')
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'password': 'test123',
+            'is_superuser': False
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().email, 'testuser@example.com')
+    
+    def test_create_user_missing_fields(self):
+        url = reverse('user-list')
+        data = {
+            # 'username': 'testuser', #missing username
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'password': 'testpassword123',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
 
 # Create your tests here.
