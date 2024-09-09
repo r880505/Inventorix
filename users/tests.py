1e4936cb74dab2ab68440141a15dbfa783e1d711
ranggi.wiharja <ranggi.wiharja@solusi247.com>
2024-09-09 14:15:30 +0000

    Added user add tests and fixed urls error

--- users/tests.py
+++ users/tests.py
@@ -2,34 +2,45 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.superuser = User.objects.create_superuser(
+            username='ranggi',
+            password='1qazxsw2'
+        )
+        
+        refresh = RefreshToken.for_user(self.superuser)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+        
     def test_create_user(self):
-        url = reverse('user-list')
+        url = reverse('user-list-create')
         data = {
             'username': 'testuser',
             'email': 'testuser@example.com',
             'first_name': 'Test',
             'last_name': 'User',
-            'password': 'test123',
-            'is_superuser': False
+            'password': '1qazxsw2'
         }
-        response = self.client.post(url, data, format='json')
+        
+        response = self.client.post(url, data, format = 'json')
+        
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().email, 'testuser@example.com')
-    
-    def test_create_user_missing_fields(self):
-        url = reverse('user-list')
+        self.assertEqual(User.objects.count(),2)
+        self.assertEqual(User.objects.get(username = 'testuser').email,'testuser@example.com')
+        
+    def test_create_user_missing_required_field(self):
+        url = reverse('user-list-create')
         data = {
-            # 'username': 'testuser', #missing username
-            'email': 'testuser@example.com',
+            # 'username': 'testuser',
+            # 'email' : 'testuser@example.com', # missing field
             'first_name': 'Test',
             'last_name': 'User',
-            'password': 'testpassword123',
+            'password': '1qazxsw2'
         }
-        response = self.client.post(url, data, format='json')
+        
+        response = self.client.post(url, data, format = 'json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(User.objects.count(), 0)
-
-# Create your tests here.
+        self.assertEqual(User.objects.count(),1)
+        
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('users/', UserListCreateView.as_view(), name='user-list'),
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
 ]
