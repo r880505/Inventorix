93acbf704f3468c792a8e5757b3dbe860ba50df5
ranggi.wiharja <ranggi.wiharja@solusi247.com>
2024-09-23 09:10:25 +0000

    Removed redundant import

--- users/models.py
+++ users/models.py
@@ -1,11 +1,9 @@
-from django.db import models
-
 import uuid
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
     def __str__(self):
-        return self.username
+        return str(self.username)
